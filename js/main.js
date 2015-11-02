ad3ee1433f042cc0e28160d2327ad1935cf9a9e7
Yutong Pan <yutonp@uw.edu>
2015-11-02 09:11:28 -0800

    Final

--- js/main.js
+++ js/main.js
@@ -5,7 +5,7 @@ var sortApp = angular.module('sortApp', []);
 
 // making sure to pass in both the $scope and $http variables
 sortApp.controller("mainController", function($scope, $http){
-
+    $scope.sortType = 'name'
 	// Use and $http.get request to get the salary data
     $http.get('data/salaries.json').then(function(response){
     // Response parameter will be an object that has a 'data' key
